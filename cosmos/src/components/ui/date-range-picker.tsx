d7b319423e4a2025e7a88e5ff12d56656bac0b57
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-11-25 14:22:41 +0530

    Admin Login,Register Frontend done

---
+++ cosmos/src/components/ui/date-range-picker.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import * as React from "react";
+import { CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { DateRange } from "react-day-picker";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+export function CalendarDateRangePicker({
+  className,
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const currentDate = new Date();
+  const [date, setDate] = React.useState<DateRange | undefined>({
+    from: currentDate,
+    to: currentDate,
+  });
+
+  return (
+    <div className={cn("grid gap-2", className)}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"outline"}
+            className={cn(
+              "w-[150px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>{format(date.from, "LLL dd, y")}</>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+      </Popover>
+    </div>
+  );
+}
