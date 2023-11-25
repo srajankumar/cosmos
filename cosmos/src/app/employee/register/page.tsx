f45d47be78cbbce890c8988e95d13c20affdc904
Tejas Nayak B <tejasnayakc42@gmail.com>
2023-11-25 14:36:23 +0530

    Employee Login,Register Frontend done

---
+++ cosmos/src/app/employee/register/page.tsx
@@ -0,0 +1,12 @@
+import Register from "@/components/employee/Register";
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Register />
+    </div>
+  );
+};
+
+export default page;
